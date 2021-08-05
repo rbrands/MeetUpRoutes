261263cf27438597fe74aa4448b51cb651466e6e
rbrands <mail@robert-brands.com>
2021-08-05 18:14:10 +0200

    Startup changed

---
+++ Client/Utils/Constants.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorApp.Client.Utils
+{
+    public static class Constants
+    {
+        public const string RoleAuthor = "author";
+        public const string RoleReviewer = "author";
+        public const string RoleTenantAdmin = "tenantadmin";
+        public const string RoleAdmin = "admin";
+        public const string RoleAllAdmins = "admin,tenantadmin";
+    }
+}
