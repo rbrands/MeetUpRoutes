e0077066943055f09bc5640236b27acb867415ba
rbrands <mail@robert-brands.com>
2021-08-05 21:32:36 +0200

    Basic plumbing

---
+++ Api/Utils/Constants.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BlazorApp.Api.Utils
+{
+    public class Constants
+    {
+        public const string HEADER_TENANT = "x-meetup-tenant";
+        public const string HEADER_TENANT_URL = "x-meetup-tenant-url";
+
+        public const string KEY_SERVER_SETTINGS = "serversettings";
+        public const string KEY_CLIENT_SETTINGS = "clientsettings";
+
+        public const string ROLE_ADMIN = "admin";
+
+
+    }
+}
