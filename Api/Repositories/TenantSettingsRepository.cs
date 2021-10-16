b4e9f0b94a3104d9e75305da18c263ae9c39d7d9
rbrands <mail@robert-brands.com>
2021-10-16 20:43:38 +0300

    Permissions

---
+++ Api/Repositories/TenantSettingsRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Azure.Cosmos;
+using System.Threading.Tasks;
+using BlazorApp.Api.Utils;
+using BlazorApp.Shared;
+
+namespace BlazorApp.Api.Repositories
+{
+    public class TenantSettingsRepository : CosmosDBRepository<TenantSettings>
+    {
+        public TenantSettingsRepository(IConfiguration config, CosmosClient cosmosClient) : base(config, cosmosClient)
+        {
+        }
+
+    }
+}
