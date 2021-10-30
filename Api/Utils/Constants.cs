5a49a391ac0452c5d4cc8e5f820dae66990cf412
rbrands <mail@robert-brands.com>
2021-10-30 18:27:12 +0200

    Ready to deploy

--- Api/Utils/Constants.cs
+++ Api/Utils/Constants.cs
@@ -12,7 +12,7 @@ namespace BlazorApp.Api.Utils
         public const string KEY_SERVER_SETTINGS = "serversettings";
         public const string KEY_CLIENT_SETTINGS = "clientsettings";
 
-        public const string VERSION = "2021-10-29";
+        public const string VERSION = "2021-10-30";
 
         public const string ROLE_ADMIN = "admin";
 
