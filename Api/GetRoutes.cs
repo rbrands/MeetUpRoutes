e69229f689af4f38e1f7f6e4bdf9f5557dd419e6
Robert Brands <rbrands@pobox.com>
2021-10-31 11:56:49 +0100

    Filter routes (#34)
    * First version with filter
    
    * Filtering improved
    
    * Filter fixed
    
    * Filter
    
    * Filter fixed
    
    * Syntax

--- Api/GetRoutes.cs
+++ Api/GetRoutes.cs
@@ -1,6 +1,8 @@
 using System;
 using System.IO;
+using System.Text;
 using System.Threading.Tasks;
+using System.Linq;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
@@ -58,38 +60,91 @@ namespace BlazorApp.Api
 
                 IEnumerable<Route> routes = null;
                 if (callingContext.IsUserConfirmed)
-                { 
+                {
                     // Get routes for review (if requested those) or only already reviewed or authored by calling user
                     if (filter.ForReview)
                     {
-                        routes = await _cosmosRepository.GetItems(r => !r.IsReviewed);
+                        if (!filter.OnlyOwn)
+                        { 
+                            routes = await _cosmosRepository.GetItems(r => r.Tenant.CompareTo(callingContext.TenantSettings.TrackKey) == 0 && !r.IsReviewed);
+                        }
+                        else
+                        {
+                            routes = await _cosmosRepository.GetItems(r => r.Tenant.CompareTo(callingContext.TenantSettings.TrackKey) == 0 && !r.IsReviewed && callingContext.User.ContactInfo.Id.CompareTo(r.AuthorId) == 0);
+                        }
                     } 
                     else 
                     {
                         if (!callingContext.IsUserReviewer)
                         {
-                            routes = await _cosmosRepository.GetItems(r => (r.IsReviewed || callingContext.User.ContactInfo.Id.CompareTo(r.AuthorId) == 0));
+                            if (!filter.OnlyOwn)
+                            { 
+                                routes = await _cosmosRepository.GetItems(r => r.Tenant.CompareTo(callingContext.TenantSettings.TrackKey) == 0 && (r.IsReviewed || callingContext.User.ContactInfo.Id.CompareTo(r.AuthorId) == 0));
+                            }
+                            else
+                            {
+                                routes = await _cosmosRepository.GetItems(r => r.Tenant.CompareTo(callingContext.TenantSettings.TrackKey) == 0 && callingContext.User.ContactInfo.Id.CompareTo(r.AuthorId) == 0);
+                            }
                         }
                         else 
                         {
-                            routes = await _cosmosRepository.GetItems();
+                            if (!filter.OnlyOwn)
+                            { 
+                                routes = await _cosmosRepository.GetItems(r => r.Tenant.CompareTo(callingContext.TenantSettings.TrackKey) == 0);
+                            }
+                            else
+                            {
+                                routes = await _cosmosRepository.GetItems(r => r.Tenant.CompareTo(callingContext.TenantSettings.TrackKey) == 0 && callingContext.User.ContactInfo.Id.CompareTo(r.AuthorId) == 0);
+                            }
                         }
                     }
                 }
                 else
                 {
                     // Get only public and reviewed routes
-                    routes = await _cosmosRepository.GetItems(r => !r.IsNonPublic && r.IsReviewed);
+                    routes = await _cosmosRepository.GetItems(r => r.Tenant.CompareTo(callingContext.TenantSettings.TrackKey) == 0 && !r.IsNonPublic && r.IsReviewed);
                 }
 
                 List<ExtendedRoute> extendedRoutes = new List<ExtendedRoute>();
                 foreach (Route r in routes)
                 {
+                    // Check filter
+                    Boolean checksPassed = true;
+                    foreach(IList<RouteTag> routeTagList in filter.Tags)
+                    {
+                        if (routeTagList.Count == 0)
+                        {
+                            continue;
+                        }
+                        checksPassed = false;
+                        foreach(RouteTag rt in routeTagList)
+                        {
+                            RouteTag foundRouteTag = r.RouteTags.FirstOrDefault(x => x.TagId.CompareTo(rt.TagId) == 0);
+                            if (null != foundRouteTag)
+                            {
+                                checksPassed = true;
+                                break;
+                            }
+                        }
+                        if (!checksPassed)
+                        {
+                            break;
+                        }
+                    }
+                    if (!checksPassed)
+                    {
+                        continue;
+                    }
+                    if (filter.OnlyForMembers && !r.IsNonPublic)
+                    {
+                        continue;
+                    }
+                    // Build ExtendedRoute
                     ExtendedRoute extendedRoute = new ExtendedRoute(r);
                     if (!String.IsNullOrEmpty(r.AuthorId))
                     { 
                         UserContactInfo author = await _cosmosUserRepository.GetItem(r.AuthorId);
-                        extendedRoute.AuthorDisplayName = author.UserNickName;
+                        extendedRoute.AuthorDisplayName = author?.UserNickName;
                         if (callingContext.IsUserReviewer)
                         {
                             extendedRoute.Author = author;
@@ -98,7 +153,7 @@ namespace BlazorApp.Api
                     if (r.IsReviewed && !String.IsNullOrEmpty(r.ReviewerId))
                     {
                         UserContactInfo reviewer = await _cosmosUserRepository.GetItem(r.ReviewerId);
-                        extendedRoute.ReviewerDisplayName = reviewer.UserNickName;
+                        extendedRoute.ReviewerDisplayName = reviewer?.UserNickName;
                         if (callingContext.IsUserReviewer)
                         {
                             extendedRoute.Reviewer = reviewer;
@@ -107,10 +162,9 @@ namespace BlazorApp.Api
                     extendedRoute.LastUpdate = r.Date;
                     extendedRoutes.Add(extendedRoute);
                 }
-                // Sort descending on date
-                extendedRoutes.Sort((x, y) => y.LastUpdate.CompareTo(x.LastUpdate));
 
-                return new OkObjectResult(extendedRoutes);
+                // return ordered descending by date and max Constants.MAX_ROUTES_COUNT items of the list.
+                return new OkObjectResult(extendedRoutes.OrderByDescending(r => r.LastUpdate).Take(Constants.MAX_ROUTES_COUNT));
             }
             catch (Exception ex)
             {
--- Shared/RouteFilter.cs
+++ Shared/RouteFilter.cs
@@ -8,8 +8,15 @@ namespace BlazorApp.Shared
     public class RouteFilter
     {
         public Boolean ForReview { get; set; }
+        public Boolean OnlyForMembers { get; set; }
+        public Boolean OnlyOwn { get; set; }
+        
         // The filter is build as a list of lists:
         // At least one tag of every inner list must be found. 
         public IList<IList<RouteTag>> Tags { get; set; }
+        public RouteFilter()
+        {
+            Tags = new List<IList<RouteTag>>();
+        }
     }
 }
