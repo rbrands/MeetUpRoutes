82864441dc5f049ef83822c375980a2ddda55b93
Robert Brands <rbrands@pobox.com>
2022-08-26 14:11:01 +0000

    Container config

---
+++ .devcontainer/postCreateCommand.sh
@@ -0,0 +1,9 @@
+## Enable local HTTPS for .NET Core
+dotnet dev-certs https --trust
+
+## Restore projects
+dotnet restore
+
+## Static Web Apps
+npm install -g @azure/static-web-apps-cli
+npm i -g azure-functions-core-tools@4 --unsafe-perm true
